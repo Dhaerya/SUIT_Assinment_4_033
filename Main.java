2563605c60cdd7ec0c819f4310272eb20eda21f9
Dhaerya <dhaeryamore@gmail.com>
2025-02-04 14:56:35 +0530

    Calling Function

--- Main.java
+++ Main.java
@@ -4,13 +4,16 @@ import java.util.*;
 class Main{
 	public static void main(String args[]){
 		Deck deck = new Deck();
-		System.out.println("*****Creating Deck*****");
+		System.out.println("*****Creating Deck\n*****");
 		deck.createDeck();
-		System.out.println("*****Print Deck*****");
+		System.out.println("*****Print Deck\n*****");
 		deck.displayDeck();
 
-		System.out.println("*****Shuffling Deck*****");
+		System.out.println("*****Shuffling Deck\n*****");
 		deck.shuffleDeck();
-		deck.displayDeck();	
+		deck.displayDeck();
+
+		System.out.println("*****Selecting Two Random Cards\n*****");		
+		deck.pickTwoRandomCards();
 	}	
 }
