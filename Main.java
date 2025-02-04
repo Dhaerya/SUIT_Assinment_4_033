1ffa8c8418943a0330b3f300755e2220db3f593d
Dhaerya <dhaeryamore@gmail.com>
2025-02-04 14:31:50 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Main.java
+	
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Deck deck = new Deck();
+		System.out.println("*****Creating Deck*****");
+		deck.createDeck();
+		System.out.println("*****Print Deck*****");
+		deck.displayDeck();
+	}	
+}
