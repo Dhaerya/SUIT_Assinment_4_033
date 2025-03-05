3df59572f5d64280b607e132380608460d8335af
Dhaerya <dhaeryamore@gmail.com>
2025-03-05 22:40:10 +0530

    Added getRank

--- Card.java
+++ Card.java
@@ -1,12 +1,16 @@
 //Card.java
 
 class Card{
-	String rank;
-	String suit;
+	private String rank;
+	private String suit;
 	
 	public Card(String rank, String suit){
-		this.rank = rank
+		this.rank = rank;
 		this.suit = suit;
+	}
+
+	public String getRank() {
+        	return rank;
 	}
 
 	public String toString(){
